6e0aaafa32a53cc98d72c860171c96b98c50cdb8
Khaled Ramadan <khaledramdan880@gmail.com>
2023-09-12 02:38:28 +0300

    Routes

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,3 +1,4 @@
+import 'package:chat_app/core/routes/routes.dart';
 import 'package:chat_app/core/theme/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -13,6 +14,8 @@ class MyApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         theme: appTheme(),
         home: const Scaffold(),
+        initialRoute: Routes.splash,
+        onGenerateRoute: AppRoutes.generateRoute,
       ),
     );
   }
